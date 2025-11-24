cd7cf6e4b698fb21f008fa2eaeef7db73bf75519
vaishnavi dhawalekar <vaishnavidhawalekar@gmail.com>
2025-11-24 06:43:50 +0530

    vpc created

--- main.tf
+++ main.tf
@@ -8,5 +8,25 @@ terraform {
 }
 
 provider "aws" {
-    region = "ap-southeast-2"
+    region = var.region
+}
+
+resource "aws_vpc" "vpc_3tier" {
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = "3-tier-vpc"
+    }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc_3tier.id
+  cidr_block              = var.pub_sub_cidr
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.vpc_3tier.id
+  cidr_block        = var.pri_sub_cidr
+  availability_zone = var.availability_zones[1]
 }
--- variable.tf
+++ variable.tf
@@ -2,5 +2,25 @@ variable "region" {
     description = "The AWS region to deploy resources in"
     type        = string
     default     = "ap-southeast-2"
-  
+}
+variable "availability_zones" {
+  description = "List of Availability Zones to use"
+  type        = list(string)
+  default     = ["ap-southeast-2a", "ap-southeast-2b"]
+}
+
+variable "vpc_cidr" {
+    description = "CIDR block for the 3-tier VPC"
+    type        = string
+    default     = "0.0.0.0/16"
+}
+variable "pub_sub_cidr" {
+    description = "CIDR block for the public subnet"
+    type        = string
+    default     = "10.0.0.0/24"
+}
+variable "pri_sub_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
 }
